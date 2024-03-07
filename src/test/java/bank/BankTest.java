64c2e062d452538f5d2b994699728c9b702f4a76
antBaena <antbaena@uma.es>
2024-03-07 18:48:20 +0100

    test de deposit y withdraw y modificacion de Withdraw

--- src/test/java/bank/BankTest.java
+++ src/test/java/bank/BankTest.java
@@ -2,16 +2,18 @@ package bank;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class BankTest {
     BankAccount bank;
+
     @BeforeEach
     void setUp() {
         bank = new BankAccount(0);
     }
 
+    @DisplayName("El metodo deposit debe añadir el dinero indicado a la cuenta")
     @Test
-    public void testPayment() {
-
+    public void Deposit_IntMoney_AddMoneyToBalance() {
 
         // Act
         int cantidad = 10000;
@@ -21,5 +23,39 @@ public class BankTest {
         assertEquals(cantidad, bank.getBalance());
     }
 
-   
+    @DisplayName("El metodo deposit debe laz una excepcion al introducir una cantidad negativa de dinero")
+    @Test
+    public void Deposit_NegativeIntMoney_ThrowsException() {
+
+        int cantidad = -10000;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bank.deposit(cantidad);
+        });
+    }
+
+    @DisplayName("El metodo withdraw debe retirar el dinero indicado de la cuenta")
+    @Test
+    public void Withdraw_IntMoney_removeMoneyFromBalance() {
+
+        // Act
+        int cantidad = 10000;
+        bank.deposit(cantidad);
+        bank.withdraw(cantidad);
+
+        // Assert
+        assertEquals(0, bank.getBalance());
+    }
+
+    @DisplayName("El metodo withdraw debe laz una excepcion al introducir una cantidad negativa de dinero")
+    @Test
+    public void Withdraw_NegativeIntMoney_ThrowsException() {
+
+        int cantidad = -10000;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            bank.withdraw(cantidad);
+        });
+    }
+
 }
